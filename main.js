ad7fdc85aba2ccd6b0b778789c62e6d305d9b01c
Servox <97719518+DoggerDoge@users.noreply.github.com>
2023-01-07 20:44:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+upperLipX = 0;
+upperLipY = 0;
+
+function preload() {
+    mustache = loadImage('mustache.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+    
+}
+
+function snapshot() {
+
+}
